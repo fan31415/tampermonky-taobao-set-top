632e5cd9e027e1f234d5cd5e75dce52f69332160
fan31415 <fan12356789@gmail.com>
2019-01-06 21:14:00 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tamper Monkey Script to facililate taobao store detail page
+# Set up
+
--- set-top.js
+++ set-top.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         edit_detail_page
+// @name         Edit_detail_page
 // @namespace    http://fanyijie.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       fan31415
 // @match        https://item.publish.tmall.com/tmall/*
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js

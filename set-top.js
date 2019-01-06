85971eea48210bcf1c9c6f49178e142d7d35dbcb
fan31415 <fan12356789@gmail.com>
2019-01-06 21:06:40 +0800

    first commit

---
+++ set-top.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         edit_detail_page
+// @namespace    http://fanyijie.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://item.publish.tmall.com/tmall/*
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// ==/UserScript==
+
+var $ = window.$;
+
+function goPC() {
+      window.location.hash = '#struct-desc'
+}
+
+function addFront() {
+    let IMG_LENGTH = 20
+    for (let index = 1; index < IMG_LENGTH; index++) {
+        $('.tm-wirless-dailog .tmall-o-image-preview').eq(index).click(function() {
+            let count = index
+            for (let j = count - 1; j >= 0; j--) {
+                $('.tm-wirless-dailog .next-icon-arrow-left').eq(j).click(function(e) {
+                    e.stopPropagation();
+                })
+                $('.tm-wirless-dailog .next-icon-arrow-left').eq(j).trigger('click');
+            }
+
+        })
+    }
+}
+function delayAdd() {
+    setTimeout(addFront, 500)
+}
+
+function addListener() {
+    $('#tm-wireless-viewArea-item_picture')[0].addEventListener("click", delayAdd);
+    console.log('All done')
+}
+$(document).ready(function(){
+  console.log("ready");
+  setTimeout(goPC, 1000)
+  setTimeout(addListener, 1500)
+
+});

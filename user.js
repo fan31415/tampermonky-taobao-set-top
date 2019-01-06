4b07ec5b767c0cdbbd78ed92ce9977fc9bcfbcad
fan31415 <fan12356789@gmail.com>
2019-01-06 21:19:23 +0800

    add user.js

---
+++ user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name          Edit_detail_page
+// @name         Edit_detail_page
+// @namespace    http://fanyijie.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       fan31415
+// @match        https://item.publish.tmall.com/tmall/*
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// ==/UserScript==
+
+// ==OpenUserJS==
+// @author fan31415
+// ==/OpenUserJS==

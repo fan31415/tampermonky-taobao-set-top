2d76458f05224f3b669fad6c8995748da48d6ca8
fan31415 <fan12356789@gmail.com>
2019-01-06 21:26:39 +0800

    add license

--- set-top.js
+++ set-top.js
@@ -1,17 +1,19 @@
 // ==UserScript==
-// @name         Edit_detail_page
+// @name         EditDetailPage
 // @namespace    http://fanyijie.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       fan31415
 // @match        https://item.publish.tmall.com/tmall/*
 // @grant        none
+// @license MIT
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // ==/UserScript==
 
 // ==OpenUserJS==
 // @author fan31415
 // ==/OpenUserJS==
+
 
 var $ = window.$;
 

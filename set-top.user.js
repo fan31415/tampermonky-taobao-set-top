30029fde22740dd23ad2db940036c20769eb471e
fan31415 <fan12356789@gmail.com>
2019-03-14 09:36:37 +0800

    update to version 4, url set to https://xiangqing.wangpu.taobao.com/ rather than tmall publish page, due to taobao update

--- set-top.user.js
+++ set-top.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         EditDetailPage
 // @namespace    http://fanyijie.net/
-// @version      0.2
+// @version      0.4
 // @description  try to take over the world!
 // @author       fan31415
-// @match        https://item.publish.tmall.com/tmall/*
+// @match        https://xiangqing.wangpu.taobao.com/*
 // @grant        none
 // @license MIT
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
@@ -25,10 +25,10 @@ function delayAddFront(index) {
 
             let count = index
             for (let j = count - 1; j >= 0; j--) {
-                $('.tm-wirless-dailog .next-icon-arrow-left').eq(j).click(function(e) {
+                $('.tm-img-media .next-icon-arrow-left').eq(j).click(function(e) {
                     e.stopPropagation();
                 })
-                $('.tm-wirless-dailog .next-icon-arrow-left').eq(j).trigger('click');
+                $('.tm-img-media .next-icon-arrow-left').eq(j).trigger('click');
             }
 
         })
@@ -36,23 +36,43 @@ function delayAddFront(index) {
 function addFront() {
     let IMG_LENGTH = 20
     for (let index = 1; index < IMG_LENGTH; index++) {
-        $('.tm-wirless-dailog .tmall-o-image-preview').eq(index).prepend('<button class="set-top" style="position:absolute;top:0;bottom:auto;text-align:center;display: block;left: 0;width: 100%;line-height: 28px;z-index:1;opacity:0;background-color:rgba(0,0,0,.6);">🔝</button>'); 
+        $('.tm-img-media .tmall-o-image-preview').eq(index).prepend('<button class="set-top" style="position:absolute;top:0;bottom:auto;text-align:center;display: block;left: 0;width: 100%;line-height: 28px;z-index:1;opacity:0;background-color:rgba(0,0,0,.6);">🔝</button>');
     }
     for (let index = 1; index < IMG_LENGTH; index++) {
       setTimeout(delayAddFront(index), 500);
     }
 }
-function delayAdd() {
-    setTimeout(addFront, 500)
+function delayAdd(myfunc) {
+    setTimeout(myfunc, 500)
 }
 
 function addListener() {
-    $('#tm-wireless-viewArea-item_picture')[0].addEventListener("click", delayAdd);
-    console.log('All done')
+    $('.wde-image-add')[0].addEventListener("click", function() { delayAdd(addFront)});
+    console.log('All listener setted')
 }
+function addMobilePageListener() {
+     $('#group1')[0].addEventListener("click", function() { delayAdd(addListener)});
+     console.log('mobilepageListener setted')
+}
+
+
 $(document).ready(function(){
   console.log("ready");
-  setTimeout(goPC, 1000)
-  setTimeout(addListener, 1500)
+//  setTimeout(goPC, 1000)
+  setTimeout(addMobilePageListener, 1500)
 
-});
+});// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();

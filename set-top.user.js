2e68c41aef503ff5162255588f320f1f38a9a3ec
fan31415 <fan12356789@gmail.com>
2019-04-12 15:20:04 +0800

    speed up script and remove verbose codes

--- set-top.user.js
+++ set-top.user.js
@@ -14,13 +14,9 @@
 // @author fan31415
 // ==/OpenUserJS==
 
-
 var $ = window.$;
 
-function goPC() {
-      window.location.hash = '#struct-desc'
-}
-function delayAddFront(index) {
+function setButtonFunc(index) {
   $('.set-top').eq(index-1).click(function() {
             var curIndex = $(this).parent().parent().parent().index()
             let count = curIndex
@@ -30,50 +26,22 @@ function delayAddFront(index) {
                 })
                 $('.tm-img-media .next-icon-arrow-left').eq(j).trigger('click');
             }
-
         })
 }
-function addFront() {
+function addSetTopButton() {
     let IMG_LENGTH = 20
     for (let index = 1; index < IMG_LENGTH; index++) {
         $('.tm-img-media .tmall-o-image-preview').eq(index).prepend('<button class="set-top" style="position:absolute;top:0;bottom:auto;text-align:center;display: block;left: 0;width: 100%;line-height: 70px;z-index:1;opacity:0;background-color:rgba(0,0,0,.6);">🔝</button>');
     }
     for (let index = 1; index < IMG_LENGTH; index++) {
-      setTimeout(delayAddFront(index), 300);
+      setTimeout(setButtonFunc(index), 100);
+      if (index == IMG_LENGTH-1) {
+          console.log('Done')
+      }
     }
 }
-function delayAdd(myfunc) {
-    setTimeout(myfunc, 300)
-}
-
-function addListener() {
-    $('.wde-image-add')[0].addEventListener("click", function() { delayAdd(addFront)});
-    console.log('All listener setted')
-}
-function addMobilePageListener() {
-     $('#group1')[0].addEventListener("click", function() { delayAdd(addListener)});
-     console.log('mobilepageListener setted')
-}
-
-
 $(document).ready(function(){
-  console.log("ready");
-//  setTimeout(goPC, 1000)
-  setTimeout(addMobilePageListener, 900)
-  setTimeout(()=>{$('#group1').click()}, 900)
-  setTimeout(()=>{$('.wde-image-add').eq(0).click()}, 1500)
-});// ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        http://*/*
-// @grant        none
-// ==/UserScript==
-
-(function() {
-    'use strict';
-
-    // Your code here...
-})();
+  $('#group1').click()
+  $('.wde-image-add').eq(0).click()
+  addSetTopButton()
+});
